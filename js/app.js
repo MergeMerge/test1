22d36f2aae95c1f8d16919915f1296c8422351ea
itcast <itcast@.cn>
2016-12-09 15:08:17 +0800

    实现展示todos列表的功能以及勾选完成项同步去除线功能

--- js/app.js
+++ js/app.js
@@ -1,6 +1,16 @@
-(function (window) {
+(function (angular) {
 	'use strict';
 
 	// Your starting point. Enjoy the ride!
-
-})(window);
+	// 1.创建模板
+	var app = angular.module('todoApp',[]);
+	// 2.创建控制器
+	app.controller('TodoController',['$scope',function($scope){
+		$scope.todoList = [
+			{id:0,name:'吃饭',isCompleted:false},
+			{id:0,name:'睡觉',isCompleted:true},
+			{id:0,name:'学习',isCompleted:false},
+			{id:0,name:'打豆豆',isCompleted:false}
+		]
+	}])
+})(angular);
